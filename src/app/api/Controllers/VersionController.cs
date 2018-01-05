ddd668c4e382952dfe430e4dd368f004bde56f86
Tomas Sundvall <tomas.sundvall@gmail.com>
2018-01-05 15:47:06 +0100

    Added sample project

---
+++ src/app/api/Controllers/VersionController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class VersionController : Controller
+    {
+        private const string VersionIdentifier = "Version";
+        private const string LastCommitIdentifier = "LastCommit";
+
+        private ILogger _logger;
+        private IHostingEnvironment _hostEnv;
+        private IConfiguration _config;
+
+        public VersionController(ILogger<VersionController> logger, IHostingEnvironment env, IConfiguration config)
+        {
+            _logger = logger;
+            _hostEnv = env;
+            _config = config;
+        }
+
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+            var version = _config[VersionIdentifier];
+            var lastCommit = _config[LastCommitIdentifier];
+
+            if ((version == null || lastCommit == null) && !_hostEnv.IsDevelopment())
+            {
+                _logger.LogWarning("If not in Dev mode, check version file.");
+            }
+
+            return new Dictionary<string, string>{
+                {"Version", version},
+                {"LastCommit:", lastCommit}
+            };
+        }
+    }
+}
---
+++ src/app/api/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webHost = new WebHostBuilder()
+                .UseKestrel(options => 
+                {
+                    options.Listen(IPAddress.Loopback, 5000);
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddJsonFile("version.json", optional: true, reloadOnChange: false);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hostingContext, log) =>
+                {
+                    log.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    log.AddConsole();
+                    log.AddDebug();
+                })
+                .UseStartup<Startup>()
+                .Build();
+
+            webHost.Run();
+        }
+    }
+}
